b569c95cb72aaea4bdbc94296e42b2dcdd76af29
Darren Greenfield <darren.greenfield@gmail.com>
2019-09-02 21:52:21 -0700

    Author and Book add forms implemented

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -1,5 +1,7 @@
 const Author = require('../models/author');
 const Book = require('../models/book');
+const { body, validationResult } = require('express-validator/check');
+const { sanitizeBody } = require('express-validator/filter');
 
 exports.author_list = (req, res, next) => {
   Author.find()
@@ -33,10 +35,67 @@ exports.author_detail = async (req, res) => {
 };
 
 exports.author_create_get = (req, res) =>
-  res.send('NOT IMPLEMENTED: Author create GET');
+  res.render('author_form', { title: 'Create Author' });
 
-exports.author_create_post = (req, res) =>
-  res.send('NOT IMPLEMENTED: Author create POST');
+exports.author_create_post = [
+  body('first_name')
+    .isLength({ min: 1 })
+    .trim()
+    .withMessage('First name must be specified.')
+    .isAlphanumeric()
+    .withMessage('First name has non-alphanumeric characters.'),
+  body('family_name')
+    .isLength({ min: 1 })
+    .trim()
+    .withMessage('Family name must be specified.')
+    .isAlphanumeric()
+    .withMessage('Family name has non-alphanumeric characters.'),
+  body('date_of_birth', 'Invalid date of birth')
+    .optional({ checkFalsy: true })
+    .isISO8601(),
+  body('date_of_death', 'Invalid date of death')
+    .optional({ checkFalsy: true })
+    .isISO8601(),
+
+  // Sanitize fields.
+  sanitizeBody('first_name').escape(),
+  sanitizeBody('family_name').escape(),
+  sanitizeBody('date_of_birth').toDate(),
+  sanitizeBody('date_of_death').toDate(),
+
+  // Process request after validation and sanitization.
+  (req, res, next) => {
+    // Extract the validation errors from a request.
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      // There are errors. Render form again with sanitized values/errors messages.
+      res.render('author_form', {
+        title: 'Create Author',
+        author: req.body,
+        errors: errors.array(),
+      });
+      return;
+    } else {
+      // Data from form is valid.
+
+      // Create an Author object with escaped and trimmed data.
+      var author = new Author({
+        first_name: req.body.first_name,
+        family_name: req.body.family_name,
+        date_of_birth: req.body.date_of_birth,
+        date_of_death: req.body.date_of_death,
+      });
+      author.save(function(err) {
+        if (err) {
+          return next(err);
+        }
+        // Successful - redirect to new author record.
+        res.redirect(author.url);
+      });
+    }
+  },
+];
 
 exports.author_delete_get = (req, res) =>
   res.send('NOT IMPLEMENTED: Author delete GET');
--- controllers/bookController.js
+++ controllers/bookController.js
@@ -2,6 +2,8 @@ const Book = require('../models/book');
 const Author = require('../models/author');
 const Genre = require('../models/genre');
 const BookInstance = require('../models/bookinstance');
+const { body, validationResult } = require('express-validator/check');
+const { sanitizeBody } = require('express-validator/filter');
 
 exports.index = async (req, res) => {
   let [
@@ -71,11 +73,89 @@ exports.book_detail = async (req, res, next) => {
     },
   });
 };
-exports.book_create_get = (req, res) =>
-  res.send('NOT IMPLEMENTED: Book create GET');
+exports.book_create_get = async (req, res, next) => {
+  let [authors, genres] = await Promise.all([Author.find(), Genre.find()]);
 
-exports.book_create_post = (req, res) =>
-  res.send('NOT IMPLEMENTED: Book create POST');
+  res.render('book_form', {
+    title: 'Create Book',
+    authors: authors,
+    genres: genres,
+  });
+};
+
+exports.book_create_post = [
+  // Convert the genre to an array.
+  (req, res, next) => {
+    if (!(req.body.genre instanceof Array)) {
+      if (typeof req.body.genre === 'undefined') req.body.genre = [];
+      else req.body.genre = new Array(req.body.genre);
+    }
+    next();
+  },
+
+  // Validate fields.
+  body('title', 'Title must not be empty.')
+    .isLength({ min: 1 })
+    .trim(),
+  body('author', 'Author must not be empty.')
+    .isLength({ min: 1 })
+    .trim(),
+  body('summary', 'Summary must not be empty.')
+    .isLength({ min: 1 })
+    .trim(),
+  body('isbn', 'ISBN must not be empty')
+    .isLength({ min: 1 })
+    .trim(),
+
+  // Sanitize fields (using wildcard).
+  sanitizeBody('*').escape(),
+
+  // Process request after validation and sanitization.
+  async (req, res, next) => {
+    // Extract the validation errors from a request.
+    const errors = validationResult(req);
+
+    // Create a Book object with escaped and trimmed data.
+    var book = new Book({
+      title: req.body.title,
+      author: req.body.author,
+      summary: req.body.summary,
+      isbn: req.body.isbn,
+      genre: req.body.genre,
+    });
+
+    if (!errors.isEmpty()) {
+      // There are errors. Render form again with sanitized values/error messages.
+
+      // Get all authors and genres for form.
+      let [authors, genres] = await Promise.all([Author.find(), Genre.find()]);
+
+      // Mark our selected genres as checked.
+      for (let i = 0; i < genres.length; i++) {
+        if (book.genre.indexOf(genres[i]._id) > -1) {
+          genres[i].checked = 'true';
+        }
+      }
+      res.render('book_form', {
+        title: 'Create Book',
+        authors: authors,
+        genres: genres,
+        book: book,
+        errors: errors.array(),
+      });
+      return;
+    } else {
+      // Data from form is valid. Save book.
+      book.save(function(err) {
+        if (err) {
+          return next(err);
+        }
+        //successful - redirect to new book record.
+        res.redirect('../../' + book.url);
+      });
+    }
+  },
+];
 
 exports.book_delete_get = (req, res) =>
   res.send('NOT IMPLEMENTED: Book delete GET');
